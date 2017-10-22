2fdb486ff4211e9323a495766e3bf968a17e44e3
oad0006 <oad0006@alu.ubu.es>
2017-10-22 22:56:45 +0200

    Desarrollo en Rama "develop" - Salida de timer
     Añadir en la clase main la salida en pantalla de la hora.

--- DesAgilPractica1/src/main/Main.java
+++ DesAgilPractica1/src/main/Main.java
@@ -12,7 +12,12 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		 System.out.println("Ejecución inicial del sistema");						 
+		 System.out.println("Ejecución inicial del sistema");		
+		 
+		  //Instanciar clase de Time
+		 Timer time = new Timer();
+		 String timeString = time.GetHour();
+		 System.out.println("Hora ejecución: "+ timeString +"\n\n");
 	}
 
 }
